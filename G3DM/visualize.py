e65d4b05777f700b703caa17372e12dd6ff9ed32
Yangyang Hu <yhu017@ucr.edu>
2021-06-09 11:09:34 -0700

    update

--- G3DM/visualize.py
+++ G3DM/visualize.py
@@ -8,7 +8,7 @@ def plot_feature(feats, position, writer, item_dir):
     data = [feats, position, feats+position]
     fig = plt.figure()
     fig, axs = plt.subplots(1, len(data))
-    cmaps = ['RdBu_r', 'viridis']
+    cmaps = ['RdBu_r', 'viridis', 'RdBu_r']
     for col in range(len(data)):
         ax = axs[col]
         pcm = ax.pcolormesh(data[col] * (col + 1), cmap=cmaps[col])
