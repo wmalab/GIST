0ccb7b7ad6dfebb098942f3e1d4b9380d56cc92c
Yangyang Hu <yhu017@ucr.edu>
2021-06-08 20:13:04 -0700

    fix

--- G3DM/visualize.py
+++ G3DM/visualize.py
@@ -9,7 +9,7 @@ def plot_feature(feats, position, writer, item_dir):
     fig, axs = plt.subplots(1, 2)
     cmaps = ['RdBu_r', 'viridis']
     for col in range(2):
-        ax = axs[0, col]
+        ax = axs[col]
         pcm = ax.pcolormesh(np.random.random((20, 20)) * (col + 1),
                             cmap=cmaps[col])
         fig.colorbar(pcm, ax=ax)
