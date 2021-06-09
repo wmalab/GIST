42ef34b28b993f8bfb1581da6bc8edcc560e2d21
Yangyang Hu <yhu017@ucr.edu>
2021-06-08 19:27:37 -0700

    add plot feature

--- G3DM/train.py
+++ G3DM/train.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 import dgl
 import torch
 import torch_optimizer as optim
@@ -12,12 +13,14 @@ from .visualize import plot_feature
 # gpuIDs = GPUtil.getAvailable(order = 'first', limit = 1, maxLoad = 0.05, maxMemory = 0.05, includeNan=False, excludeID=[], excludeUUID=[])
 # device =  'cpu' if len(gpuIDs)==0 else 'cuda:{}'.format(gpuIDs[0])
 
+
 def load_dataset(path, name):
     '''graph_dict[chromosome] = {top_graph, top_subgraphs, bottom_graph, inter_graph}
     feature_dict[chromosome] = {'hic_feat_h0', 'hic_feat_h1'}
     HiCDataset[i]: graph[i], feature[i], label[i](chromosome)'''
-    HiCDataset = torch.load(os.path.join( path, name))
+    HiCDataset = torch.load(os.path.join(path, name))
     return HiCDataset
+
 
 def create_network(configuration, graph, device):
     config = configuration['parameter']
@@ -31,25 +34,29 @@ def create_network(configuration, graph, device):
         {('h0_bead', 'interacts_0', 'h0_bead'): int(sampling_num['l0'])},
         {('h0_bead', 'interacts_0', 'h0_bead'): int(sampling_num['l1'])},
         {('h0_bead', 'interacts_0', 'h0_bead'): int(sampling_num['l2'])}])  # ,('h1_bead', 'center_1_0', 'h0_bead'):0
-    
+
     ind0 = int(config['feature']['in_dim']['h0'])
     outd0 = int(config['feature']['out_dim']['h0'])
     ind1 = int(config['feature']['in_dim']['h1'])
     outd1 = int(config['feature']['out_dim']['h1'])
     em_h0_bead = embedding(ind0+ind1, outd0).to(device)
     em_h1_bead = embedding(ind1, outd1).to(device)
-    
+
     nh0 = int(config['G3DM']['num_heads']['0'])
     nh1 = int(config['G3DM']['num_heads']['1'])
     nhout = int(config['G3DM']['num_heads']['out'])
-    
+
     chain = config['G3DM']['graph_chain_dim']
-    cin, chidden, cout = int(chain['in_dim']), int(chain['hidden_dim']), int(chain['out_dim'])
-    e_list = ['interacts_1_c{}'.format(i) for i in np.arange(int(config['graph']['cutoff_cluster']))]
-    en_chain_net = encoder_chain(cin, chidden, cout, num_heads=nh1, etypes=e_list).to(device)
+    cin, chidden, cout = int(chain['in_dim']), int(
+        chain['hidden_dim']), int(chain['out_dim'])
+    e_list = ['interacts_1_c{}'.format(i) for i in np.arange(
+        int(config['graph']['cutoff_cluster']))]
+    en_chain_net = encoder_chain(
+        cin, chidden, cout, num_heads=nh1, etypes=e_list).to(device)
 
     bead = config['G3DM']['graph_bead_dim']
-    bdin, bdhidden, bdout = int(bead['in_dim']), int(bead['hidden_dim']), int(bead['out_dim'])
+    bdin, bdhidden, bdout = int(bead['in_dim']), int(
+        bead['hidden_dim']), int(bead['out_dim'])
 
     en_bead_net = encoder_bead(bdin, bdhidden, bdout).to(device)
     en_union = encoder_union(in_h1_dim=cout, in_h0_dim=bdout, out_dim=3,
@@ -72,16 +79,19 @@ def create_network(configuration, graph, device):
                          amsbound=False,
                          )
     em_networks = [em_h0_bead, em_h1_bead]
-    ae_networks = [en_chain_net, en_bead_net, en_union, de_center_net, de_bead_net]
+    ae_networks = [en_chain_net, en_bead_net,
+                   en_union, de_center_net, de_bead_net]
     return sampler, em_networks, ae_networks, nll, opt
+
 
 def setup_train(configuration):
     itn = int(configuration['parameter']['G3DM']['iteration'])
     batch_size = int(configuration['parameter']['G3DM']['batchsize'])
     return itn, batch_size
 
+
 def fit_one_step(graphs, features, sampler, batch_size, em_networks, ae_networks, loss_fc, optimizer, device):
-    top_graph= graphs['top_graph'].to(device)
+    top_graph = graphs['top_graph'].to(device)
     top_subgraphs = graphs['top_subgraphs'].to(device)
     bottom_graph = graphs['bottom_graph']
     inter_graph = graphs['inter_graph'].to(device)
@@ -94,7 +104,8 @@ def fit_one_step(graphs, features, sampler, batch_size, em_networks, ae_networks
     en_union = ae_networks[2]
     de_center_net, de_bead_net = ae_networks[3], ae_networks[4]
 
-    eid_dict = {etype: bottom_graph.edges(etype=etype, form='eid') for etype in bottom_graph.etypes}
+    eid_dict = {etype: bottom_graph.edges(
+        etype=etype, form='eid') for etype in bottom_graph.etypes}
     dataloader = dgl.dataloading.EdgeDataLoader(bottom_graph, {'interacts_0': eid_dict['interacts_0']}, sampler, device=device,
                                                 batch_size=batch_size, shuffle=True, drop_last=True)
     top_list = [e for e in top_subgraphs.etypes if 'interacts_1_c' in e]
@@ -104,18 +115,21 @@ def fit_one_step(graphs, features, sampler, batch_size, em_networks, ae_networks
     for input_nodes, pair_graph, blocks in dataloader:
         blocks = [b.to(device) for b in blocks]
         X1 = em_h1_bead(h1_feat)
-        h_center = en_chain_net(top_subgraphs, X1, top_list, ['w'], ['h1_bead'])
+        h_center = en_chain_net(
+            top_subgraphs, X1, top_list, ['w'], ['h1_bead'])
 
-        inputs0 = torch.tensor(h0_feat[input_nodes.cpu().detach(),:], dtype=torch.float).to(device)  # ['h0_bead']
+        inputs0 = torch.tensor(h0_feat[input_nodes.cpu().detach(
+        ), :], dtype=torch.float).to(device)  # ['h0_bead']
         X0 = em_h0_bead(inputs0)
         h_bead = en_bead_net(blocks, X0, ['interacts_0'], ['w'])
 
-        h0 = dgl.in_subgraph(inter_graph, {'h0_bead': blocks[2].dstnodes()}).edges()[1]  # dst
+        h0 = dgl.in_subgraph(inter_graph, {'h0_bead': blocks[2].dstnodes()}).edges()[
+            1]  # dst
         h0, _ = torch.sort(torch.unique(h0))
-        h1 = dgl.in_subgraph(inter_graph, {'h0_bead': blocks[2].dstnodes()}).edges()[0]  # src
+        h1 = dgl.in_subgraph(inter_graph, {'h0_bead': blocks[2].dstnodes()}).edges()[
+            0]  # src
         h1, _ = torch.sort(torch.unique(h1))
         inter = dgl.node_subgraph(inter_graph, {'h0_bead': h0, 'h1_bead': h1})
-
 
         c = h_center[h1, :, :].to(device)
         res = en_union(inter, c, h_bead)
@@ -136,11 +150,10 @@ def fit_one_step(graphs, features, sampler, batch_size, em_networks, ae_networks
         return loss_list
 
 
-def run_epoch(dataset, model, loss_fc, optimizer, sampler, batch_size, iterations, device):
+def run_epoch(dataset, model, loss_fc, optimizer, sampler, batch_size, iterations, device, writer=None):
     em_networks, ae_networks = model
     loss_list = []
 
-    writer = torch.utils.tensorboard.SummaryWriter()
     for i in np.arange(iterations):
         for j, data in enumerate(dataset):
             graphs, features, _ = data
@@ -150,12 +163,11 @@ def run_epoch(dataset, model, loss_fc, optimizer, sampler, batch_size, iteration
             h1_f = features['hic_h1']['feat']
             h1_p = features['hic_h1']['pos']
             h1_feat = torch.tensor(h1_f + h1_p, dtype=torch.float).to(device)
-            
-            ll = fit_one_step(graphs, [h0_feat, h1_feat], sampler, batch_size, em_networks, ae_networks, loss_fc, optimizer, device)
+
+            ll = fit_one_step(graphs, [h0_feat, h1_feat], sampler, batch_size,
+                              em_networks, ae_networks, loss_fc, optimizer, device)
             loss_list.append(ll)
 
-            if i%5 == 0 and j==0:
+            if i % 5 == 0 and j == 0 and writer is not None:
                 plot_feature(None, None, writer)
         print("epoch {:d} Loss {:f}".format(i, np.mean(loss_list)))
-    writer.close()
-
--- test_train.py
+++ test_train.py
@@ -1,22 +1,25 @@
-import time,os
+import time
+import os
 import json
 import dgl
 import torch
 import GPUtil
 
 import numpy as np
-from G3DM.train import load_dataset, create_network, setup_train,run_epoch
+from G3DM.train import load_dataset, create_network, setup_train, run_epoch
 
 import warnings
 warnings.filterwarnings('ignore')
 
-gpuIDs = GPUtil.getAvailable(order = 'first', limit = 1, maxLoad = 0.05, maxMemory = 0.05, includeNan=False, excludeID=[], excludeUUID=[])
-device =  torch.device('cpu' if len(gpuIDs)==0 else 'cuda:{}'.format(gpuIDs[0]))
+gpuIDs = GPUtil.getAvailable(order='first', limit=1, maxLoad=0.05,
+                             maxMemory=0.05, includeNan=False, excludeID=[], excludeUUID=[])
+device = torch.device('cpu' if len(gpuIDs) ==
+                      0 else 'cuda:{}'.format(gpuIDs[0]))
 print(device)
 
 if __name__ == '__main__':
 
-    # root= os.path.join('.') # 
+    # root= os.path.join('.') #
     root = os.path.join('/rhome/yhu/bigdata/proj/experiment_G3DM')
     configuration_src_path = os.path.join(root, 'data')
     configuration_name = 'config.json'
@@ -26,17 +29,23 @@ if __name__ == '__main__':
     cool_file = config_data['cool_file']
     cell = cool_file.split('.')[0]
     hyper = '_'.join([cool_file.split('.')[1], 'id', config_data["id"]])
-    
+
     # '/rhome/yhu/bigdata/proj/experiment_G3DM'
     root = config_data['root'] if config_data['root'] else root
-    cool_data_path = config_data['cool_data_path'] if config_data['cool_data_path'] else os.path.join( root, 'data', 'raw')
-    graph_path = config_data['graph_path'] if config_data['graph_path'] else os.path.join( root, 'data', cell, hyper, 'graph')
-    feature_path = config_data['feature_path'] if config_data['feature_path'] else os.path.join( root, 'data', cell, hyper, 'feature')
-    dataset_path = config_data['dataset_path']['path'] if config_data['dataset_path']['path'] else os.path.join( root, 'data', cell, hyper)
+    cool_data_path = config_data['cool_data_path'] if config_data['cool_data_path'] else os.path.join(
+        root, 'data', 'raw')
+    graph_path = config_data['graph_path'] if config_data['graph_path'] else os.path.join(
+        root, 'data', cell, hyper, 'graph')
+    feature_path = config_data['feature_path'] if config_data['feature_path'] else os.path.join(
+        root, 'data', cell, hyper, 'feature')
+    dataset_path = config_data['dataset_path']['path'] if config_data['dataset_path']['path'] else os.path.join(
+        root, 'data', cell, hyper)
     dataset_name = config_data['dataset_path']['name'] if config_data['dataset_path']['name'] else 'dataset.pt'
-    output_path = config_data['output_path'] if config_data['output_path'] else os.path.join( root, 'data', cell, hyper, 'output')
+    output_path = config_data['output_path'] if config_data['output_path'] else os.path.join(
+        root, 'data', cell, hyper, 'output')
 
-    saved_model_path = config_data['saved_model']['path'] if config_data['saved_model']['path'] else os.path.join( root, 'saved_model')
+    saved_model_path = config_data['saved_model']['path'] if config_data['saved_model']['path'] else os.path.join(
+        root, 'saved_model')
     saved_model_name = config_data['saved_model']['name'] if config_data['saved_model']['name'] else 'model_net'
 
     '''ratio = config_data['hyper']['ratio']
@@ -59,14 +68,21 @@ if __name__ == '__main__':
 
     cutoff_cluster = int(config_data['parameter']['graph']['cutoff_cluster'])'''
 
-    # load dataset 
+    # load dataset
     HiCDataset = load_dataset(dataset_path, dataset_name)
     graphs, features, label = HiCDataset[0]
 
     # creat network model
-    sampler, em_networks, ae_networks, nll, opt = create_network(config_data, graphs, device)
+    sampler, em_networks, ae_networks, nll, opt = create_network(
+        config_data, graphs, device)
 
-    # setup and call train 
+    # setup and call train
     itn, batch_size = setup_train(config_data)
-    run_epoch(HiCDataset, [em_networks, ae_networks], nll, opt, sampler, batch_size, itn, device)
-    
+    log_dir = config_data['log_dir'] if config_data['log_dir'] else os.path.join(
+        root, 'log', cell, hyper)
+    os.makedirs(log_dir, exist_ok=True)
+    writer = torch.utils.tensorboard.SummaryWriter(log_dir)
+    run_epoch(HiCDataset, [em_networks, ae_networks],
+              nll, opt, sampler,
+              batch_size, itn, device, writer)
+    writer.close()
