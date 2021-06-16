1c0240be520fc104e4a89ed9daeae2e173addd16
Yangyang Hu <yhu017@ucr.edu>
2021-06-16 12:40:53 -0700

    update

--- G3DM/loss.py
+++ G3DM/loss.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+# import torch.nn.functional as F
 
 class RMSLELoss(nn.Module):
     def __init__(self):
@@ -15,11 +15,12 @@ class nllLoss(torch.nn.Module):
         super(nllLoss, self).__init__()
     
     def forward(self, pred, target, weights=None):
-        logp = F.log_softmax(pred, 1)
-        loss = F.nll_loss(logp, target.long(), reduce=True, reduction='mean')
+        logp = torch.nn.functional.log_softmax(pred, 1)
         if weights is not None:
             w = weights/weights.sum()
-            loss = loss*w[target.int8()]
+            loss = torch.nn.functional.nll_loss(logp, target.long(), weight=w, reduce=True, reduction='mean')
+        else:
+            loss = torch.nn.functional.nll_loss(logp, target.long(), reduce=True, reduction='mean')
         return loss
 
 class crossNllLoss(nn.Module):
@@ -30,10 +31,10 @@ class crossNllLoss(nn.Module):
         class_p = torch.argmax(pred, dim=-1, keepdim=False)
         class_t = torch.argmax(target, dim=-1, keepdim=False)
 
-        logp = F.log_softmax(pred, 1)
-        logt = F.log_softmax(target, 1)
-        loss_pt = F.nll_loss(logp, class_t.long(), reduce=True, reduction='mean')
-        loss_pp = F.nll_loss(logp, class_p.long(),  reduce=True, reduction='mean')
-        loss_tt = F.nll_loss(logt, class_t.long(), reduce=True, reduction='mean')
+        logp = nn.log_softmax(pred, 1)
+        logt = nn.log_softmax(target, 1)
+        loss_pt = nn.nll_loss(logp, class_t.long(), reduce=True, reduction='mean')
+        loss_pp = nn.nll_loss(logp, class_p.long(),  reduce=True, reduction='mean')
+        loss_tt = nn.nll_loss(logt, class_t.long(), reduce=True, reduction='mean')
         loss = loss_pt + loss_pp + loss_tt
         return loss
