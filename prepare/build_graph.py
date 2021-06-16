85ddfbe91351515775054d44cfd141280b013f88
Yangyang Hu <yhu017@ucr.edu>
2021-06-15 17:48:54 -0700

    debug

--- prepare/build_graph.py
+++ prepare/build_graph.py
@@ -52,6 +52,7 @@ def create_hierarchical_graph_2lvl(norm_hics, num_clusters, ratios, strides, cut
         m, pb = cluster_hic(hic, hic, n_cluster=num_clusters[i])
         mats_.append(m)
         matpbs_.append(pb)
+        print('mat: ', m.max(), num_clusters[i])
         cluster_weight, _ = np.histogram(m.view(-1,1), bins=np.arange(num_clusters[i]))
         cluster_weight = cluster_weight/cluster_weight.sum()
         cweights_.append(cluster_weight)
