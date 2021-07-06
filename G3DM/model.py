1d9582170203f3511c30174bfb01291f70a65bf6
Yangyang Hu <yhu017@ucr.edu>
2021-07-06 12:58:57 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -265,8 +265,7 @@ class MergeLayer(torch.nn.Module):
 
     def reduce_func(self, nodes):
         alpha = torch.nn.functional.softmax(nodes.mailbox['e'], dim=1)
-        h = torch.sum(alpha * (nodes.mailbox['src_z'] - nodes.mailbox['dst_z']), dim=1, keepdim=True) + nodes.mailbox['dst_z']
-        h = torch.squeeze(h, dim=1)
+        h = torch.sum(alpha * (nodes.mailbox['src_z']), dim=1)
         return {'ah': h}
 
     def forward(self, graph, h0, h1):
