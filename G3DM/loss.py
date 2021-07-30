074b313f8b7b11f5d5909792819c1eec6790d000
Yangyang Hu <yhu017@ucr.edu>
2021-07-29 21:04:02 -0700

    update stdloss

--- G3DM/loss.py
+++ G3DM/loss.py
@@ -55,8 +55,8 @@ class stdLoss(nn.Module):
         
     def forward(self, std, cluster, num_cluster):
         # cluster = torch.argmax(pred, dim=-1)
-        weight = torch.abs(cluster - num_cluster/2)**2 - (num_cluster/6)**2
-        res = torch.mean(torch.exp(std*weight.view(-1,1)))
+        weight = torch.abs(cluster - num_cluster/2) - (num_cluster/6)
+        res = torch.sqrt(torch.mean(torch.exp(std*weight.view(-1,1))))
         return res
 
 class nllLoss(torch.nn.Module):
