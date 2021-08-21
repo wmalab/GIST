36d8ee505ca729924720f5064bf84f3accfa584e
Yangyang Hu <yhu017@ucr.edu>
2021-08-21 16:02:35 -0700

    RAdam

--- G3DM/train.py
+++ G3DM/train.py
@@ -50,7 +50,7 @@ def create_network(configuration, device):
     # opt = optim.AdaBound(list(em_bead.parameters()) + list(en_net.parameters()) + list(de_net.parameters()),
     #                     lr=2*1e-3, betas=(0.9, 0.999), final_lr=0.1, gamma=1e-3, eps=1e-8, weight_decay=0,
     #                     amsbound=False)
-    opt = optim.DiffGrad(list(em_bead.parameters()) + list(en_net.parameters()) + list(de_net.parameters()),
+    opt = optim.RAdam(list(em_bead.parameters()) + list(en_net.parameters()) + list(de_net.parameters()),
                     lr= 1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0,
                     )
 
