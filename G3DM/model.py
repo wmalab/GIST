cf41bb3784b57e5f10dc1923d82638f833eae38d
Yangyang Hu <yhu017@ucr.edu>
2021-08-25 22:43:38 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -546,8 +546,8 @@ class decoder_gmm(torch.nn.Module):
         r_dist = self.distance_means.clamp(min=0.1)
         dis_ms = torch.cumsum(r_dist, dim=0).clamp(min=0.5) - (r_dist/2.0)
 
-        k = torch.sigmoid(self.k.clamp(min=-4.0, max=4.0))
-        std = torch.relu(torch.div(r_dist, 2.0*torch.sqrt(torch.tensor(2.0))*k )) + 1e-5
+        k = torch.sigmoid(self.k.clamp(min=-4.0, max=4.0))+0.5
+        std = torch.relu(torch.div(r_dist, torch.sqrt(torch.tensor(2.0))*k )) + 1e-5
         dis_cmp = D.Normal( torch.relu(dis_ms), std)
         dis_gmm = D.MixtureSameFamily(mix, dis_cmp)
 
