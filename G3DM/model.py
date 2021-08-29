a5418edd5e0839c79d1d6a85a7247d43716ae5df
Yangyang Hu <yhu017@ucr.edu>
2021-08-28 19:13:58 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -544,7 +544,8 @@ class decoder_gmm(torch.nn.Module):
         # d_right = torch.cat( (torch.zeros(1, device=d_right.device), d_right), dim=0)
         # means = (d_left + d_right)
 
-        means, idx = torch.relu(torch.sort( self.means) )
+        means, idx = torch.sort( self.means)
+        means = torch.relu(means)
         stds = (torch.relu(self.distance_stdevs) + 1e-1)[idx]
         dis_cmp = D.Normal( means, stds)
         dis_gmm = D.MixtureSameFamily(mix, dis_cmp)
