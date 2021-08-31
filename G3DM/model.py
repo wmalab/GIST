29c64a09e757ddb3529aa16502a500f78c5fc2e1
lhanappa <allen.huyangyang@gmail.com>
2021-08-31 16:08:14 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -508,7 +508,7 @@ class decoder_gmm(torch.nn.Module):
         self.weights = torch.nn.Parameter( torch.ones( (self.num_clusters)), requires_grad=True)
         # self.k = torch.nn.Parameter( torch.ones(self.num_clusters), requires_grad=True)
 
-        ms = torch.sqrt(torch.linspace(0., 5.0, steps=self.num_clusters, dtype=torch.float))
+        ms = torch.sqrt(torch.linspace(0., 9.0, steps=self.num_clusters, dtype=torch.float))
         self.means = torch.nn.Parameter( ms, requires_grad=True)
         self.distance_stdevs = torch.nn.Parameter( torch.ones( (self.num_clusters)), requires_grad=True)
 
@@ -534,8 +534,8 @@ class decoder_gmm(torch.nn.Module):
         # d_right = torch.cat( (torch.zeros(1, device=d_right.device), d_right), dim=0)
         # means = (d_left + d_right)
 
-        # means = torch.sqrt( torch.relu(self.means) )
-        means = torch.nn.LeakyReLU(0.1)(self.means)
+        means = torch.sqrt( torch.relu(self.means) )
+        # means = torch.nn.LeakyReLU(0.1)(self.means)
         means = means.clamp(max=5.0) + self.interval 
 
         stds = (torch.relu(self.distance_stdevs) + 1e-3)
