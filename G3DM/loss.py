e4dc01822f49e408c847246e4f40b0961aed30c3
Yangyang Hu <yhu017@ucr.edu>
2021-08-30 21:33:51 -0700

    update

--- G3DM/loss.py
+++ G3DM/loss.py
@@ -18,7 +18,7 @@ class ClusterWassersteinLoss(nn.Module):
         diff = pred_cdf - target_cdf
         res = (torch.abs(diff)).mean(dim=0)
 
-        w = (cw/cw.mean())*torch.sqrt(cw/cw.mean()+1.0) 
+        w = (cw/cw.mean() +10.0) # *torch.sqrt(cw/cw.mean()+1.0) 
         w = self.num_cluster*torch.nn.functional.normalize(w.view(1,-1), p=1)
         res = res.view(1,-1)*w.view(1,-1)
         res = res.sum(dim=-1)
