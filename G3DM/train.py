b39f38bc959cdafa772867ca3dabd4e31f3ea8d9
lhanappa <allen.huyangyang@gmail.com>
2021-09-06 14:13:16 -0700

    update

--- G3DM/train.py
+++ G3DM/train.py
@@ -54,14 +54,14 @@ def create_network(configuration, device):
                 list(de_distance_net.parameters()) + \
                 list(de_gmm_net.parameters())
 
-    # opt = optim.AdaBound( list(em_bead.parameters()) + list(en_net.parameters()) 
-    #                     + list(de_distance_net.parameters()) + list(de_gmm_net.parameters()),
-    #                     lr=1e-3, betas=(0.9, 0.999), final_lr=0.1, gamma=1e-3, eps=1e-8, weight_decay=0,
-    #                     amsbound=False)
+    opt = optim.AdaBound( list(em_bead.parameters()) + list(en_net.parameters()) 
+                        + list(de_distance_net.parameters()) + list(de_gmm_net.parameters()),
+                        lr=1e-3, betas=(0.9, 0.999), final_lr=0.1, gamma=1e-3, eps=1e-8, weight_decay=0,
+                        amsbound=False)
 
-    opt = optim.RAdam( parameters_list,
-                        lr= 1e-3, betas=(0.9, 0.999),
-                        eps=1e-8, weight_decay=0)
+    # opt = optim.RAdam( parameters_list,
+    #                     lr= 1e-3, betas=(0.9, 0.999),
+    #                     eps=1e-8, weight_decay=0)
 
     # opt = optim.QHAdam( parameters_list,
     #                     lr= 1e-3, betas=(0.9, 0.999),
