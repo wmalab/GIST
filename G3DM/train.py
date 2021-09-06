e05bba39e73efb7d0d1994f5c232b1bfb8244810
lhanappa <allen.huyangyang@gmail.com>
2021-09-06 11:31:15 -0700

    update

--- G3DM/train.py
+++ G3DM/train.py
@@ -54,6 +54,11 @@ def create_network(configuration, device):
                         lr=1e-3, betas=(0.9, 0.999), final_lr=0.1, gamma=1e-3, eps=1e-8, weight_decay=0,
                         amsbound=False)
 
+    opt = optim.RAdam( list(em_bead.parameters()) + list(en_net.parameters()) 
+                        + list(de_distance_net.parameters()) + list(de_gmm_net.parameters()),
+                        lr= 1e-3, betas=(0.9, 0.999),
+                        eps=1e-8, weight_decay=0)
+
     # opt = torch.optim.RMSprop(list(em_bead.parameters()) + list(en_net.parameters()) 
     #                         + list(de_distance_net.parameters()) + list(de_gmm_net.parameters()))
 
