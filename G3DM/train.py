6e92999725e707293d313fa10284a9c8077b28d2
lhanappa <allen.huyangyang@gmail.com>
2021-09-06 14:21:18 -0700

    adaBound

--- G3DM/train.py
+++ G3DM/train.py
@@ -54,16 +54,13 @@ def create_network(configuration, device):
                 list(de_distance_net.parameters()) + \
                 list(de_gmm_net.parameters())
 
-    # opt = optim.AdaBound( parameters_list, 
-    #                     lr=1e-3, betas=(0.9, 0.999), 
-    #                     final_lr=0.1, gamma=1e-3, 
-    #                     eps=1e-8, weight_decay=0,
-    #                     amsbound=False)
-
-    opt = optim.NovoGrad(parameters_list,
-                        lr= 1e-3, betas=(0.9, 0.999),
+    opt = optim.AdaBound( parameters_list, 
+                        lr=1e-3, betas=(0.9, 0.999), 
+                        final_lr=0.1, gamma=1e-3, 
                         eps=1e-8, weight_decay=0,
-                        grad_averaging=False, amsgrad=False)
+                        amsbound=False)
+
+
     # opt = optim.RAdam( parameters_list,
     #                     lr= 1e-3, betas=(0.9, 0.999),
     #                     eps=1e-8, weight_decay=0)
