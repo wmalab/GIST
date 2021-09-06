eeade3e5bb87477a1c0ebcbd40c3ee7111bad3dc
lhanappa <allen.huyangyang@gmail.com>
2021-09-05 17:23:16 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -512,7 +512,7 @@ class decoder_gmm(torch.nn.Module):
         # ms = torch.linspace(-.1, 4.3, steps=self.num_clusters, dtype=torch.float, requires_grad=True)
         # self.means = torch.nn.Parameter( ms, requires_grad=True)
         
-        ms = torch.linspace(0.5, 2, steps=self.num_clusters, dtype=torch.float, requires_grad=True)
+        ms = torch.linspace(0.5, 1.5, steps=self.num_clusters, dtype=torch.float, requires_grad=True)
         self.distance_stdevs = torch.nn.Parameter( ms**2, requires_grad=True)
 
         inter = torch.linspace(start=0, end=0.01, steps=self.num_clusters, device=self.distance_stdevs.device)
@@ -521,7 +521,7 @@ class decoder_gmm(torch.nn.Module):
 #    # gmm
     def fc(self, stds_l, stds_r, k):
         k = torch.sigmoid(k.clamp(min=-9.0, max=9.0))
-        k = k.clamp(min=0.5)
+        k = k.clamp(min=0.6)
         rate = torch.div(stds_l, stds_r)
         kr = (k*rate) # must < 1
         return stds_l * torch.sqrt( -2.0 * torch.log(kr) )
@@ -564,7 +564,7 @@ class decoder_gmm(torch.nn.Module):
 
         data = torch.log(distance).view(-1,1)
         # data = distance.view(-1,1)
-        data = data.clamp(max=6.0) - 6.0
+        data = data.clamp(max=5.0) - 5.0
         data = (-1.0 * data) #.clamp(max=8.0) # + 6.0
         unsafe_dis_cmpt_lp = dis_gmm.component_distribution.log_prob(data)
 
