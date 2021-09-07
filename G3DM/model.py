3fb147c63af18ef604e505f824f8e22829f5a3bc
lhanappa <allen.huyangyang@gmail.com>
2021-09-06 18:59:50 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -530,7 +530,7 @@ class decoder_gmm(torch.nn.Module):
 
     def forward(self, distance, cweight):
         # cweight = torch.nn.functional.normalize(cweight.view(-1,), p=1, dim=0)
-        cweight = torch.softmax(cweight)
+        cweight =  torch.nn.functional.softmax(cweight, 1)
         mix = D.Categorical( cweight)
 
         # stds, idx = torch.sort(torch.relu(self.distance_stdevs).view(-1,), dim=0)
