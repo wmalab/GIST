403d76b20935f649ed542b598707a28842873a5f
lhanappa <allen.huyangyang@gmail.com>
2021-09-12 17:15:45 -0700

    update

--- G3DM/model.py
+++ G3DM/model.py
@@ -535,7 +535,7 @@ class decoder_gmm(torch.nn.Module):
         # means = torch.nan_to_num(means, nan=6.0)
         # means = means + self.interval
 
-        # stds = torch.relu(self.distance_stdevs) + 1e-3
+        stds = torch.relu(self.distance_stdevs) + 1e-3
         # mode = torch.exp(means - stds**2)
         # _, idx = torch.sort(mode.view(-1,), dim=0, descending=False)
         # means = means[idx]
