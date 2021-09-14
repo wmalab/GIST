beb359145f6d9921a565d698f0fbb3b91cf37791
lhanappa <allen.huyangyang@gmail.com>
2021-09-14 14:40:30 -0700

    adabound True

--- G3DM/train.py
+++ G3DM/train.py
@@ -143,10 +143,10 @@ def fit_one_step(epoch, require_grad, graphs, features, cluster_weights, em_netw
         idx = ((lt == i).nonzero(as_tuple=True)[0]).view(-1,)
         p = torch.ones_like(idx, dtype=float)/idx.shape[0]
         # ids.append(idx[p.multinomial(num_samples=int( torch.minimum(n[i], torch.tensor(idx.shape[0])) ), replacement=False)])
-        ids.append(idx[p.multinomial(num_samples=int( torch.minimum(n[i], torch.tensor(idx.shape[0]) ), replacement=False)])
+        ids.append(idx[ p.multinomial( num_samples=int( torch.minimum(n[i], torch.tensor(idx.shape[0])) ), replacement=False)])
     mask = torch.cat(ids, dim=0)
-    # mask, _ = torch.sort(mask)
-    mask = torch.unique(mask, sorted=True, return_inverse=False, return_counts=False)
+    mask, _ = torch.sort(mask)
+    # mask = torch.unique(mask, sorted=True, return_inverse=False, return_counts=False)
 
     sample_dis_cmpt_lp = dis_cmpt_lp[mask, :]
     sample_lt = lt[mask]
