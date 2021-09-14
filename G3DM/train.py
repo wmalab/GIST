f10432551a4edbc33e82892f0d196c6a89883e6c
lhanappa <allen.huyangyang@gmail.com>
2021-09-14 11:32:50 -0700

    fix

--- G3DM/train.py
+++ G3DM/train.py
@@ -125,7 +125,7 @@ def fit_one_step(epoch, require_grad, graphs, features, cluster_weights, em_netw
     for i in torch.arange(ncluster):
         idx = ((lt == i).nonzero(as_tuple=True)[0]).view(-1,)
         p = torch.ones_like(idx, dtype=float)/idx.shape[0]
-        ids.append(idx[p.multinomial(num_samples=int(torch.mininum(n[i], len(idx))), replacement=False)])
+        ids.append(idx[p.multinomial(num_samples=int(torch.minimum(n[i], len(idx))), replacement=False)])
     mask = torch.cat(ids, dim=0)
     mask, _ = torch.sort(mask)
     # mask = torch.unique(mask, sorted=True, return_inverse=False, return_counts=False)
