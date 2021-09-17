e2f5c87f14ec462aef09901fba92faceef0938e1
lhanappa <allen.huyangyang@gmail.com>
2021-09-17 16:12:22 -0700

    update

--- G3DM/fit.py
+++ G3DM/fit.py
@@ -226,6 +226,7 @@ def run_epoch(datasets, model, loss_fc, optimizer, scheduler, iterations, device
             h_f_n = torch.nn.functional.normalize(torch.tensor(h_f), p=1.0, dim=1)*h_f.shape[1]
             h_p_n =torch.nn.functional.normalize(torch.tensor(h_p), p=1.0, dim=1)*h_p.shape[1]
             h_feat = torch.stack([h_f_n, h_p_n], dim=1).to(device)
+            h_feat = h_feat.float()
 
             ll, dis_gmm = fit_one_step(True, graphs, h_feat, lr_ranges, em_networks, ae_networks, loss_fc, optimizer, device)
             mu = dis_gmm.component_distribution.mean.detach()
