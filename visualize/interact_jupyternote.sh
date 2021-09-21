dd439d400b1283c6db3aa67f72779322b9417848
lhanappa <allen.huyangyang@gmail.com>
2021-09-21 10:41:18 -0700

    update config

---
+++ visualize/interact_jupyternote.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# # Create a virtual environment, if you don't already have one
+# conda create -n ipykernel_py2 python=2 ipykernel
+
+# # Load the new environment
+# conda activate ipykernel_py2
+
+# # Install kernel
+# python -m ipykernel install --user --name myenv --display-name "JupyterPy2"
