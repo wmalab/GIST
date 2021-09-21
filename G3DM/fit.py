970ed52dae647345c8031af3ac81cc88c099db8c
lhanappa <allen.huyangyang@gmail.com>
2021-09-20 18:31:01 -0700

    fix

--- G3DM/fit.py
+++ G3DM/fit.py
@@ -446,7 +446,7 @@ def run_prediction(dataset, model, saved_parameters_model, num_heads, num_cluste
         [true_cluster_mat, dis_gmm]] = predict(graphs, h_feat, num_heads, num_clusters, em_networks, ae_networks, device)
         prediction[index] = {'structures': pred_X, 
                             'predict_cluster': [pred_dist_cluster_mat, pdcm_list], 
-                            'predict_distance': [pred_dit_mat, pdm_list],
+                            'predict_distance': [pred_dist_mat, pdm_list],
                             'true_cluster': true_cluster_mat}
         prediction['mixture model'] = dis_gmm
         dur = time.time() - t0
