619aeb6c36e752b7ceee50949e6f3dc89b79ceb2
lhanappa <allen.huyangyang@gmail.com>
2021-10-14 10:45:16 -0700

    add comparison

---
+++ comparison/install_methods.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+date
+hostname
+source activate env_cmp
+
+# pastis
+pip install pastis
+
+# Shrec3d-extened
+https://github.com/jbmorlot/ShRec-Exented.git
+
+# GEM
+https://github.com/GuangxiangZhu/GEM.git
+
+# ChromSDE
+gwet https://www.comp.nus.edu.sg/~bioinfo/ChromSDE/ChromSDE_program2.2.zip
+unzip ChromSDE_program2.2.zip -d ./
