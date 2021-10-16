58f5308f7996274e841b81b0110b6629bd804310
lhanappa <allen.huyangyang@gmail.com>
2021-10-16 13:30:50 -0700

    test demo

--- comparison/run_gem.m
+++ comparison/run_gem.m
@@ -4,7 +4,7 @@ function run_gem(input_path)
     folder_dir = "/rhome/yhu/bigdata/proj/experiment_G3DM/chromosome_3D/comparison/GEM"
     % Add that folder plus all subfolders to the path.
     addpath(genpath(folder_dir));
-    x = importdata( fullfile( input_path, "norm_mat.txt") );
-    bin = importdata( fullfile( input_path, "loci.txt") )
+    x = fullfile( input_path, "norm_mat.txt")
+    bin = fullfile( input_path, "loci.txt")
     GEM(x, bin, 1E4, 10, 5E12, 0, -1);
 end
