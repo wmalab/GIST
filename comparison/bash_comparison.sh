f61a6480376e579b51058bf07cd540b0adb0c97a
lhanappa <allen.huyangyang@gmail.com>
2021-11-07 19:16:02 -0800

    add chromsde

--- comparison/bash_comparison.sh
+++ comparison/bash_comparison.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-chrom=(X)  # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X)
-method=(lordg pastis gem shrec3d)
+chrom=(20 21 22)  # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X)
+method=(chromsde) # lordg pastis gem shrec3d
 for m in "${method[@]}"
 do
     for i in "${chrom[@]}"
